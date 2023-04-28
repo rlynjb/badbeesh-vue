ac05965259e66630f6ca19a327f5a7b8c122ad47
rlyn <rlynjb@gmail.com>
2023-04-28 07:35:08 -0700

    fixed header style

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="container w-6/12 mt-8 mb-8">
+  <header class="container mt-8 mb-8">
     <div class="grid grid-cols-5">
       <div class="logo col-span-1">
         Badbeesh
